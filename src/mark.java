8e89f933aa06a47ac833152a8bc3df86db84f39a
SilvioSilva <silviosilva.ads@gmail.com>
2019-10-08 16:39:12 -0300

    Funções Matematicas

--- src/mark.java
+++ src/mark.java
@@ -4,6 +4,10 @@ public class mark {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Bom dia!");
+int x = 9, y;
+y = x;
+System.out.println(y + x);
+
 	}
 
 }
